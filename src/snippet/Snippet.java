2f72d9f968158cedf37f2648dca53a2867f53876
Finaritra <rfina@hotmail.fr>
2015-10-06 09:12:19 +0200

    suppression de snipped et junittest.java

--- src/snippet/Snippet.java
+++
@@ -1,8 +0,0 @@
-package snippet;
-
-public class Snippet {
-	public static void main(String[] args) {
-		//C:\Users\bendaoud\git\SIT\SIT213-TransmissionGitHub\SIT213-ChaineTransmission\.classpath
-	}
-}
-
