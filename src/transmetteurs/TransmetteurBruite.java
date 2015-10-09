eb8052ef68008e034ecd2af1de202de9dffa88a2
JordanMl <jordan.souls@gmail.com>
2015-10-09 09:46:53 +0200

    Modif TransmetteurBruite
    Le bruit est bien ajouté au signal :+1:  . Reste à faire ->
    - modifier les méthodes dans TransmetteurBruite pour faciliter les
    tests?
    - modifier le récepteur
    - vérifier que le bruit est gaussien

--- src/transmetteurs/TransmetteurBruite.java
+++ src/transmetteurs/TransmetteurBruite.java
@@ -44,33 +44,28 @@ public class TransmetteurBruite extends Transmetteur<Float, Float> {
 		float cacheCalcul = 0;
 		int i =0; 
 		
+		informationEmise = informationRecue; //Copie du signal recu dans informationEmise
+		
 		// Calcul de Pb en fonction du SNR et de Ps
 		puissanceMoyBruit = (float) (10*Math.log10(puissanceMoySignalRecu)-snr);
 		puissanceMoyBruit = (float) Math.pow(10, puissanceMoyBruit);	
 		System.out.println("Puissance moyenne du bruit en fonction de Ps :"+ puissanceMoyBruit);
 		
 		// Boucle : Ajout du bruit calculeï¿½ chaque echantillon (voir formule)
-		for (Float echantillon : informationRecue){
-			a1 = (float) Math.random();
+		for (Float echantillon : informationEmise){
+			a1 = (float) Math.random(); //a1 et a2 les des variables aléatoires du calcul
 			a2 = (float) Math.random();
+			
 		    //Calcul d'un echantillon de bruit
-			// Remettre le calcul en forme (Jordan)
-			System.out.println("a1 = "+ a1 +"// a2 = "+ a2);
+			/*    Garder pour l'instant le calcul détaillé ici tant qu'on a pas vérifié que c'était gaussien
 			echantillonBruite = (float) (Math.sqrt(puissanceMoyBruit));
-			System.out.println("Racine de la puissance moyenne en entrée :"+ echantillonBruite);
 			cacheCalcul = (float) (-2*Math.log(1-a1));
-			System.out.println("Cache calcul -2ln(1-a1) : "+ cacheCalcul);
 			echantillonBruite = (float) Math.sqrt(cacheCalcul);
-			System.out.println("Multiplication par racine de -2ln(1-a1) :"+ echantillonBruite);
-			echantillonBruite = (float) (echantillonBruite*Math.cos(2*Math.PI*a2));
-			System.out.println("Multiplication par cos(2PIa2) :"+ echantillonBruite);
-			System.out.println("Echantillon bruite calcule :"+ echantillonBruite);
-			informationRecue.setIemeElement(i, informationRecue.iemeElement(i)+ echantillonBruite);
+			echantillonBruite = (float) (echantillonBruite*Math.cos(2*Math.PI*a2)); */
+			echantillonBruite = (float) (Math.sqrt(puissanceMoyBruit)*Math.sqrt(-2*Math.log(1-a1))*Math.cos(2*Math.PI*a2));
+			informationEmise.setIemeElement(i, informationEmise.iemeElement(i)+ echantillonBruite);
 			echantillon = echantillon + echantillonBruite; //Somme des échantillons du signal recue avec le bruit
-			System.out.println("valeur de i :"+ i);
-			i++;
-			//informationEmise.add(echantillonBruite);
-			//echantillonBruite = (float) (Math.sqrt(puissanceMoyBruit)*Math.sqrt(-2*Math.log1p(1-a1))*Math.cos(2*Math.PI*a2));
+			i++; 
 		}
 		
 	}
@@ -79,12 +74,9 @@ public class TransmetteurBruite extends Transmetteur<Float, Float> {
 	public void recevoir(Information<Float> information) throws InformationNonConforme {
 		informationRecue = information; //Reception de l'information 
 		System.out.println("---- Information recue => "+ informationRecue.toString());
-		calculPuissanceMoySignal ();
-		bruitBlancGaussien();
-		
-		// Appel methode Calcul de la puissance du signal recu PS
-		// Appel methode Calcul du Bruit en fonction de PS et du SNR
-		emettre(); //Emission de l'information bruitï¿½e vers le rï¿½cepteur		
+		calculPuissanceMoySignal (); //Calcul la puissance moyenne d'informationRecue
+		bruitBlancGaussien(); //Calcul des échantillons de bruit blanc et l'ajoute au signal à émettre
+		emettre(); //Emission de l'information bruitee vers le recepteur		
 	}
 
 	@Override
