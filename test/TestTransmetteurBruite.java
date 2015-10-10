4538dd1188899b22721bab062be56ffb10d8f799
Bendaoud <bendaoud@bendaoud-PC.fr>
2015-10-10 15:42:15 +0200

    Ajout d'un file pour les tests (contient test pour Transmetteur et simulateur

---
+++ test/TestTransmetteurBruite.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author bendaoud
+ *
+ */
+public class TestTransmetteurBruite {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link transmetteurs.TransmetteurBruite#calculPuissanceMoySignal()}.
+	 */
+	@Test
+	public void testCalculPuissanceMoySignal() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link transmetteurs.TransmetteurBruite#bruitBlancGaussien()}.
+	 */
+	@Test
+	public void testBruitBlancGaussien() {
+		fail("Not yet implemented");
+	}
+
+}
