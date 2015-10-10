43f65862384eeb106665d35ea4c0bd6375b781dd
Bendaoud <bendaoud@bendaoud-PC.fr>
2015-10-10 16:56:26 +0200

    Ajout du test TestCalculPuissanceMoyenne

--- test/TestTransmetteurBruite.java
+++ test/TestTransmetteurBruite.java
@@ -1,7 +1,17 @@
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
+import information.Information;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import destinations.Destination;
+import destinations.DestinationFinale;
+import sources.Source;
+import sources.SourceFixe;
+import transmetteurs.*;
 
 /**
  * 
@@ -13,11 +23,19 @@ import org.junit.Test;
  */
 public class TestTransmetteurBruite {
 
+	Source source ;
+	Emetteur emetteur ;
+	Recepteur recepteur ;
+	Destination  destination ;
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		
+		
+		
 	}
 
 	/**
@@ -25,7 +43,34 @@ public class TestTransmetteurBruite {
 	 */
 	@Test
 	public void testCalculPuissanceMoySignal() {
-		fail("Not yet implemented");
+		//test Basique
+		TransmetteurBruite transmetteur = new TransmetteurBruite(1.0f);
+		Information<Float> info =  new Information<Float>();
+		info.add(2.0f);
+		info.add(3.0f);
+		info.add(4.0f);
+		info.add(-1.0f);
+		info.add(0.0f);
+		assertEquals(6.0f,transmetteur.calculPuissanceMoySignalRecu(info),0.0f);
+		
+		//Si le signal reçu est nul
+		info =  new Information<Float>();
+		info.add(0.0f);
+		info.add(0.0f);
+		info.add(0.0f);
+		info.add(0.0f);
+		info.add(0.0f);
+		assertEquals(0.0f,transmetteur.calculPuissanceMoySignalRecu(info),0.0f);
+		
+		//Si le signal reçu est trés grand
+		info =  new Information<Float>();
+		int i=0;
+		for(i=0;i<10000;i++){
+			info.add(2.0f);
+		}
+		
+		assertEquals(4.0f,transmetteur.calculPuissanceMoySignalRecu(info),0.0f);
+		
 	}
 
 	/**
@@ -33,7 +78,7 @@ public class TestTransmetteurBruite {
 	 */
 	@Test
 	public void testBruitBlancGaussien() {
-		fail("Not yet implemented");
+		
 	}
 
 }
