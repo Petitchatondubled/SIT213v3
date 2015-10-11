8d244e6d95148ef483f06414b98c983403e3d264
Finaritra <rfina@hotmail.fr>
2015-10-11 19:07:08 +0200

    ajustement de répertoire

--- src/launchHistogramm.sh
+++ src/launchHistogramm.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp ".:jcommon-1.0.23.jar:jfreechart-1.0.19.jar"  test/histogramm $1 $2 $3 $4 \&& java -cp ".:jcommon-1.0.23.jar:jfreechart-1.0.19.jar" test/NormalDistribution
+java -cp ".:../jcommon-1.0.23.jar:../jfreechart-1.0.19.jar"  test/histogramm $1 $2 $3 $4 \&& java -cp ".:../jcommon-1.0.23.jar:../jfreechart-1.0.19.jar" test/NormalDistribution
