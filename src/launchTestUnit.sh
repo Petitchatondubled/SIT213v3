9caa1ec6783ab815e98f1fb7fc27384d52f7e571
Finaritra <rfina@hotmail.fr>
2015-10-11 19:08:03 +0200

    ajustement répértoire

--- src/launchTestUnit.sh
+++ src/launchTestUnit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-java -cp ".:jcommon-1.0.23.jar:jfreechart-1.0.19.jar:junit-4.10.jar:.."  org.junit.runner.JUnitCore test.TestSimulateur
-java -cp ".:jcommon-1.0.23.jar:jfreechart-1.0.19.jar:junit-4.10.jar:.."  org.junit.runner.JUnitCore test.TestTransmetteurBruite
+java -cp ".:../jcommon-1.0.23.jar:../jfreechart-1.0.19.jar:../junit-4.10.jar:.."  org.junit.runner.JUnitCore test.TestSimulateur
+java -cp ".:../jcommon-1.0.23.jar:../jfreechart-1.0.19.jar:../junit-4.10.jar:.."  org.junit.runner.JUnitCore test.TestTransmetteurBruite
 
 
