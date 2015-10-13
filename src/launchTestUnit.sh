16b2fa5963def9943c923351d62cc2208e9ef844
Petitchatondubled <bendaoud.moha@hotmail.fr>
2015-10-13 16:08:07 +0200

    Update launchTestUnit.sh

--- src/launchTestUnit.sh
+++ src/launchTestUnit.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-javac test/TestSimulateur.java test/
-javac test/TestTransmetteurBruite.java test/
+dir
 java -cp ".:../junit-4.10.jar:../test"  org.junit.runner.JUnitCore TestSimulateur
 java -cp ".:../junit-4.10.jar:../test"  org.junit.runner.JUnitCore TestTransmetteurBruite
 
