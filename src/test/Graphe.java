da37c41df7ef074d359d2f9622b7df1a24b3b881
Bendaoud <bendaoud@bendaoud-PC.fr>
2015-10-15 14:17:23 +0200

    ajout classe graphe pour tracer des graphiques

---
+++ src/test/Graphe.java
@@ -0,0 +1,152 @@
+package test;
+
+
+
+import information.Information;
+
+import java.awt.Color;
+import java.awt.Shape;
+import java.awt.geom.Rectangle2D;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.DefaultDrawingSupplier;
+import org.jfree.chart.plot.DrawingSupplier;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.*;
+
+import com.sun.javafx.scene.paint.GradientUtils.Parser;
+
+/**
+ * A simple demonstration application showing how to create a line chart using data from an
+ * {@link XYDataset}.
+ *
+ */
+public class Graphe extends ApplicationFrame {
+	
+	final XYSeriesCollection dataset;
+
+    /**
+     * Creates a new demo.
+     *
+     * @param title  the frame title.
+     */
+    public Graphe(final String title) {
+
+        super(title);
+        this.setName(title);
+        dataset = new XYSeriesCollection();
+
+    }
+    
+    /**
+     * Creates a sample dataset.
+     * 
+     * @return a sample dataset.
+     */
+    public void createDataset(String name,Information<Float> xInfo,Information<Float> yInfo) {
+        
+        final XYSeries series1 = new XYSeries(name);
+        
+        for(int i=0;i<xInfo.nbElements();i++){
+        	series1.add(xInfo.iemeElement(i), yInfo.iemeElement(i));
+        }
+        dataset.addSeries(series1);
+        
+        
+    }
+    
+    /**
+     * Creates a chart.
+     * 
+     * @param dataset  the data for the chart.
+     * 
+     * @return a chart.
+     */
+    JFreeChart createChart(final XYDataset dataset,String axeX,String axeY) {
+        
+        // create the chart...
+        final JFreeChart chart = ChartFactory.createXYLineChart(
+            this.getName(),      // chart title
+            axeX,                      // x axis label
+            axeY,                      // y axis label
+            dataset,                  // data
+            PlotOrientation.VERTICAL,
+            true,                     // include legend
+            true,                     // tooltips
+            false                     // urls
+        );
+
+        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+        chart.setBackgroundPaint(Color.white);
+       
+//        final StandardLegend legend = (StandardLegend) chart.getLegend();
+  //      legend.setDisplaySeriesShapes(true);
+        
+        // get a reference to the plot for further customisation...
+        final XYPlot plot = chart.getXYPlot();
+        Shape[] shapes = new Shape[1];
+        shapes[0] = new Rectangle2D.Double(-2, -2, 2, 2);
+
+        final DrawingSupplier supplier = new DefaultDrawingSupplier(
+            DefaultDrawingSupplier.DEFAULT_PAINT_SEQUENCE,
+            DefaultDrawingSupplier.DEFAULT_OUTLINE_PAINT_SEQUENCE,
+            DefaultDrawingSupplier.DEFAULT_STROKE_SEQUENCE,
+            DefaultDrawingSupplier.DEFAULT_OUTLINE_STROKE_SEQUENCE,
+            shapes
+        );
+     
+       supplier.getNextShape();
+       supplier.getNextShape();
+        plot.setBackgroundPaint(Color.lightGray);
+    //    plot.setAxisOffset(new Spacer(Spacer.ABSOLUTE, 5.0, 5.0, 5.0, 5.0));
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        plot.setDrawingSupplier(supplier);
+        final XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesLinesVisible(0, false);
+        renderer.setSeriesShapesVisible(1, false);
+        plot.setRenderer(renderer);
+
+//        // change the auto tick unit selection to integer units only...
+//        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+//        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+//        // OPTIONAL CUSTOMISATION COMPLETED.
+//                
+        return chart;
+        
+    }
+
+    // ****************************************************************************
+    // * JFREECHART DEVELOPER GUIDE                                               *
+    // * The JFreeChart Developer Guide, written by David Gilbert, is available   *
+    // * to purchase from Object Refinery Limited:                                *
+    // *                                                                          *
+    // * http://www.object-refinery.com/jfreechart/guide.html                     *
+    // *                                                                          *
+    // * Sales are used to provide funding for the JFreeChart project - please    * 
+    // * support us so that we can continue developing free software.             *
+    // ****************************************************************************
+    
+    /**
+     * Starting point for the demonstration application.
+     *
+     * @param args  ignored.
+     */
+//    public static void main(final String[] args) {
+//
+//        final Graphe demo = new Graphe("Line Chart Demo 6");
+//        demo.pack();
+//        RefineryUtilities.centerFrameOnScreen(demo);
+//        demo.setVisible(true);
+//
+//    }
+
+}
