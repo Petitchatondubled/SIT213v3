4d924fe43355ae098956fba516b6e9c6d6b57463
Finaritra <rfina@hotmail.fr>
2015-10-18 22:57:57 +0200

    Update launchSimuBER.Sh

--- src/launchSimuBER.sh
+++ src/launchSimuBER.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "lancement de la simulation du BER en fonction du SNR"
-java test.courbeTEB $1 $2 $3 $4 $5 $6 $7 $8
+java -cp ".:../jcommon-1.0.23.jar:../jfreechart-1.0.19.jar" test.courbeTEB $1 $2 $3 $4 $5 $6 $7 $8
